9cbf79621a522c302d5c0f929ef6a03f96048b48
NoriX <versatility.95@mail.ru>
2019-09-03 15:23:11 +0300

    Tests with simulating actions

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,21 @@ it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'some new comment' }
+    });
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('some new comment');
+});
+
+it('should be cleared after submitting', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'some new comment' }
+    });
+    wrapped.update();
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+})
